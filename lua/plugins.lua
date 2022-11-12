021b0ffbae059c85152e7f65267cee559cffb05a
argon <hello@alexarcher.me>
2022-11-12 20:47:26 +0300

    existing files commit

---
+++ lua/plugins.lua
@@ -0,0 +1,195 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+
+    -----------------------------------------------------------
+    -- ПЛАГИНЫ ВНЕШНЕГО ВИДА
+    -----------------------------------------------------------
+
+    -- Цветовая схема
+    use 'ii14/onedark.nvim'
+
+    --- Информационная строка внизу
+    use { 'nvim-lualine/lualine.nvim',
+      requires = 'nvim-tree/nvim-web-devicons',
+      config = function()
+        require('lualine').setup()
+      end,
+    }
+
+    -- Табы вверху
+    use { 'akinsho/bufferline.nvim',
+      tag = 'v3.*',
+      requires = 'nvim-tree/nvim-web-devicons',
+      config = function() require'bufferline'.setup {} end
+    }
+
+
+    -----------------------------------------------------------
+    -- НАВИГАЦИЯ
+    -----------------------------------------------------------
+    -- Файловый менеджер
+    use { 'nvim-tree/nvim-tree.lua',
+      requires = 'nvim-tree/nvim-web-devicons',
+      cmd = "NvimTreeToggle",
+      config = function() require'nvim-tree'.setup {} end
+    }
+
+
+    -- Навигация внутри файла по классам и функциям
+    use 'majutsushi/tagbar'
+    -- Замена fzf и ack
+    use { 'nvim-telescope/telescope.nvim',
+      requires = 'nvim-lua/plenary.nvim',
+      config = function() require'telescope'.setup {} end 
+    }
+
+
+    -----------------------------------------------------------
+    -- LSP и автодополнялка
+    -----------------------------------------------------------
+
+
+    -- Highlight, edit, and navigate code using a fast incremental parsing library
+    use 'nvim-treesitter/nvim-treesitter'
+    -- Collection of configurations for built-in LSP client
+    use 'neovim/nvim-lspconfig'
+    use 'williamboman/nvim-lsp-installer'
+    -- Автодополнялка
+    use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-buffer'
+    use 'saadparwaiz1/cmp_luasnip'
+    --- Автодополнлялка к файловой системе
+    use 'hrsh7th/cmp-path'
+    -- Snippets plugin
+    use 'L3MON4D3/LuaSnip'
+
+
+    -----------------------------------------------------------
+    -- HTML и CSS
+    -----------------------------------------------------------
+    -- Подсвечивает закрывающий и откры. тэг. Если, где-то что-то не закрыто, то не подсвечивает.
+    use 'idanarye/breeze.vim'
+    -- Закрывает автоматом html и xml тэги. Пишешь <h1> и он автоматом закроется </h1>
+    use 'alvan/vim-closetag'
+    -- Подсвечивает #ffffff
+    use 'ap/vim-css-color'
+
+    -----------------------------------------------------------
+    -- РАЗНОЕ
+    -----------------------------------------------------------
+    -- Даже если включена русская раскладка vim команды будут работать
+    use 'powerman/vim-plugin-ruscmd'
+    -- 'Автоформатирование' кода для всех языков
+    use 'Chiel92/vim-autoformat'
+    -- ]p - вставить на строку выше, [p - ниже
+    use 'tpope/vim-unimpaired'
+    -- Переводчик рус - анг   л
+    use 'skanehira/translate.vim'
+    --- popup окошки
+    use 'nvim-lua/popup.nvim'
+    -- Обрамляет или снимает обрамление. Выдели слово, нажми S и набери <h1>
+    use 'tpope/vim-surround'
+    -- Считает кол-во совпадений при поиске
+    use 'google/vim-searchindex'
+    -- Может повторять через . vimsurround
+    use 'tpope/vim-repeat'
+    -- Стартовая страница, если просто набрать vim в консоле
+    use 'mhinz/vim-startify'
+    -- Комментирует по gc все, вне зависимости от языка программирования
+    use { 'numToStr/Comment.nvim',
+    config = function() require('Comment').setup() end }
+    -- Обрамляет строку в теги по ctrl- y + ,
+    use 'mattn/emmet-vim'
+    -- Закрывает автоматом скобки
+    use 'cohama/lexima.vim'
+    -- Линтер, работает для всех языков
+    use 'dense-analysis/ale'
+
+end)
+
+--[[
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use 'kyazdani42/nvim-web-devicons'
+
+  use 'scrooloose/nerdtree'
+
+  use '9mm/vim-closer'
+
+  use 'tibabit/vim-templates'
+
+  use {'michal-h21/vim-zettel',
+    requires = {'vimwiki/vimwiki','junegunn/fzf','junegunn/fzf.vim'}
+  }
+
+  use {'jpalardy/vim-slime'}
+
+  -- Load on specific commands
+  use {'tpope/vim-dispatch', opt = true, cmd = {'Dispatch', 'Make', 'Focus', 'Start'}}
+
+  -- Load on an autocommand event
+  use {'andymass/vim-matchup', event = 'VimEnter'}
+
+  -- Load on a combination of conditions: specific filetypes or commands
+  -- Also run code after load (see the "config" key)
+  use {
+    'w0rp/ale',
+    ft = {'sh', 'zsh', 'bash', 'c', 'cpp', 'cmake', 'html', 'markdown', 'racket', 'vim', 'tex'},
+    cmd = 'ALEEnable',
+    config = 'vim.cmd[[ALEEnable] ]'
+  }
+
+  -- Plugins can have dependencies on other plugins
+  use {
+    'haorenW1025/completion-nvim',
+    opt = true,
+    requires = {{'hrsh7th/vim-vsnip', opt = true}, {'hrsh7th/vim-vsnip-integ', opt = true}}
+  }
+
+  -- You can specify rocks in isolation
+  use_rocks 'penlight'
+  use_rocks {'lua-resty-http', 'lpeg'}
+
+  -- Plugins can have post-install/update hooks
+  use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd = 'MarkdownPreview'}
+
+  -- Post-install/update hook with neovim command
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', opt = true }
+
+  -- Post-install/update hook with call of vimscript function with argument
+  use { 'glacambre/firenvim', run = function() vim.fn['firenvim#install'](0) end }
+
+  -- Use specific branch, dependency and run lua file after load
+  use {
+    'glepnir/galaxyline.nvim',
+	branch = 'main', 
+--	config = function() require('statusline') end,
+	requires = {'kyazdani42/nvim-web-devicons'}
+  }
+
+  -- Galaxyline Color
+  use {'Avimitin/nerd-galaxyline'}
+
+
+  -- Use dependency and run lua function after load
+  use {
+    'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
+    config = function() require('gitsigns').setup() end
+  }
+
+  -- You can specify multiple plugins in a single call
+  use {'tjdevries/colorbuddy.vim'}
+
+  -- You can alias plugin names
+  use {'dracula/vim', as = 'dracula'}
+
+  -- Fugitive for Git
+  use { 'tpope/vim-fugitive' }
+
+end)
+]]--
